5025780b517c947b5fcc262041974e0f7cd4ba75
uopi <iljapvlv@gmail.com>
2025-10-24 23:05:07 +0500

    подправил первый тест

--- usue-online-tests/Tests/LinSpace/LinSpace01.cs
+++ usue-online-tests/Tests/LinSpace/LinSpace01.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using NuGet.ContentModel;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using Test_Wrapper;
 
@@ -9,21 +11,24 @@ namespace usue_online_tests.Tests.PolynomA
     public class LinSpace01 : ITestCreator, ITest, ITestGroup
     {
         public int TestID { get; set; }
-        public string Name { get; } = "Умножение многочленов";
-        public string Description { get; } = "Умножение многочленов от одной переменой";
-        public string GroupName { get; set; } = "PolynomA";
+        public string Name { get; } = "Определение подпространства01";
+        public string Description { get; } = "Определение подпространства";
+        public string GroupName { get; set; } = "LinSpace";
         private static readonly char[] LetterU = { 'U', 'V', 'W', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
         private static readonly char[] LetterA = { 'p', 'q', 'r', 'x', 'y', 'z', 't' };
         public ITest CreateTest(int randomSeed)
         {
-
+            var rand = new Random(randomSeed);
+            Span<char> letters = GenLetters(randomSeed);
+            char letterU = letters[0];
+            char letterA = letters[1];
 
             ITest result = new LinSpace01();
             result.Text =
                 $"Пусть {letterU} - линейное пространство для многочленов степени, не выше 3. " +
                 $"Отметьте щелчком мыши в поле для ввода те пункты, в которых приведено" +
                 $" характеристическое свойство F({letterA}(x)) подмножества " +
-                $"\\(\\{{{letterA}(x) \\| F({letterA}(x))\\}}, определите подпространство:\n";
+                $"\\(\\{{{letterA}(x) | F({letterA}(x))\\}}, определите подпространство:\\)\n";
 
             result.CheckBoxes = GenFormuls(randomSeed);
             
@@ -33,19 +38,13 @@ namespace usue_online_tests.Tests.PolynomA
 
         public int CheckAnswer(int randomSeed, Dictionary<string, string> answers)
         {
+            int[] nums = GenNum(randomSeed).Take(3).ToArray();
+
             string[] questions = GenFormuls(randomSeed);
             var total = 0;
 
-            for (int i=0; i<answers.Count; i++)
-            {
-                if ()
-            }
-            //if (answers.TryGetValue("Ce", out var userAnswer4) &&
-            //    int.TryParse(userAnswer4, out var userNumber4) &&
-            //    userNumber4 == Ce)
-            //{
-            //    total++;
-            //}
+            for (int i = 0; i < 6; i++)
+                if (answers.TryGetValue(questions[i], out var val) && val == "on" && nums.Contains(i + 1)) total++;
 
             return total;
         }
@@ -53,8 +52,14 @@ namespace usue_online_tests.Tests.PolynomA
         private string[] GenFormuls(int seed)
         {
             
+            Span<char> letters = GenLetters(seed);
+            Span<int> CD = GenNum(seed);
+            int Cx = CD[6];
+            int Cy = CD[7];
+            int Dx = CD[9];
+            int Dy = CD[10];
+            int Dz = CD[11];
 
-            Span<char> letters = GenLetters(random);
             char letterU = letters[0];
             char letterA = letters[1];
 
@@ -68,8 +73,9 @@ namespace usue_online_tests.Tests.PolynomA
                 $"\\({Cy}\\cdot {letterA}({Cx}) + {Dx}\\cdot {letterA}({Dy}) = {Dz}\\)"
             };
         }
-        private char[] GenLetters(Random random)
+        private char[] GenLetters(int seed)
         {
+            var random = new Random(seed);
             return new char[] 
             { 
                 LetterU[random.Next(0, LetterU.Length)],
@@ -78,7 +84,7 @@ namespace usue_online_tests.Tests.PolynomA
         }
         private int[] GenNum(int randomSeed)
         {
-            var random = new Random(seed);
+            var random = new Random(randomSeed);
             List<int> listAB = new List<int> { 1, 2, 3, 4, 5, 6 };
             List<int> listCD = new List<int> { -9, -8, -7, -6, -5, -4, -3, -2, 2, 3, 4, 5, 6, 7, 8, 9 };
 
@@ -105,7 +111,14 @@ namespace usue_online_tests.Tests.PolynomA
             int Dy = listCD[random.Next(0, listCD.Count)];
             listCD.Remove(Dy);
             int Dz = listCD[random.Next(0, listCD.Count)];
-            return new { };
+            return new int[]{ 
+
+                            Ax, Ay, Az,
+                            Bx, By, Bz,
+            
+                            Cx, Cy, Cz,
+                            Dx, Dy, Dz
+            };
         }
         public string Text { get; set; }
         public string[] CheckBoxes { get; set; }
